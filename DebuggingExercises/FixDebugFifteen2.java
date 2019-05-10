3b5191ed0e04c7fc6771f6ce37408522c5e931c7
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-05-10 13:09:55 +0800

    complete debugging exercise

---
+++ DebuggingExercises/FixDebugFifteen2.java
@@ -0,0 +1,32 @@
+// Demonstrates layout positions
+// using BorderLayout
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class FixDebugFifteen2 extends JFrame
+{
+   JButton nb = new JButton("Left    ");
+   JButton sb = new JButton("Down  ");
+   JButton eb = new JButton("Right ");
+   JButton wb = new JButton("Center  ");
+   JButton cb = new JButton("Up");
+   Container con = null;
+
+   public FixDebugFifteen2()
+   {
+      con = this.getContentPane();
+      con.setLayout(new BorderLayout());
+
+       con.add(nb, BorderLayout.WEST);
+       con.add(sb, BorderLayout.SOUTH);
+       con.add(eb, BorderLayout.EAST);
+       con.add(wb, BorderLayout.CENTER);
+       con.add(cb, BorderLayout.NORTH);
+   }
+   public static void main(String[] args)
+   {
+      FixDebugFifteen2 f = new FixDebugFifteen2();
+      f.setSize(300, 300);
+      f.setVisible(true);
+   }
+}
